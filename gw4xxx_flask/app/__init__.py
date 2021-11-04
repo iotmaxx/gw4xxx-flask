92b1a020269645c82fbed2c2a4ee766a38f61378
Ralf Glaser <glaser@iotmaxx.de>
2021-11-04 13:26:50 +0100

    GW41FLASK-7: add server name

--- gw4xxx_flask/app/__init__.py
+++ gw4xxx_flask/app/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_restful import Api, Resource
 
 theApplication = Flask(__name__)
+theApplication.config['SERVER_NAME'] = '192.168.1.1:5000'
+theApi = Api(theApplication)
 
-from app import routes
+from gw4xxx.deviceInfo import DeviceInfoAPI
+
+theApi.add_resource(DeviceInfoAPI, '/', endpoint='gw4xxx_deviceInfo')
+
+#from app import routes
 
