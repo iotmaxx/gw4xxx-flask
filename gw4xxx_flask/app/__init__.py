9d7f5b5bf57d0f7d6126f327945af07b3dc44e57
Ralf Glaser <glaser@iotmaxx.de>
2021-11-16 13:57:20 +0100

    GW41FLASK-11: use request context for url_for

--- gw4xxx_flask/app/__init__.py
+++ gw4xxx_flask/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restful import Api, Resource
 
 theApplication = Flask(__name__)
-theApplication.config['SERVER_NAME'] = '192.168.2.1:5000'
+#theApplication.config['SERVER_NAME'] = '192.168.1.1:5000'
 theApi = Api(theApplication)
 
 from gw4xxx.deviceInfo import DeviceInfoAPI
