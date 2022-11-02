6433a30a0242dbc19ea4b93cdba8dfae3b1c4540
Ralf Glaser <glaser@iotmaxx.de>
2022-11-02 11:02:01 +0100

    api version 0.1.8

--- gw4xxx_flask/app/__init__.py
+++ gw4xxx_flask/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api, Resource
 
-apiVersion = "0.1.7"
+apiVersion = "0.1.8"
 theApplication = Flask(__name__)
 #theApplication.config['SERVER_NAME'] = '192.168.1.1:5000'
 theApi = Api(theApplication)
